a6b5ed558aca269f352e4501642e1799c6148695
第一次上传测试 <1578684787@qq.com>
2022-02-14 15:57:57 +0800

    2、部分bug修改

--- app/src/main/java/com/example/noteapp/feature_note/domain/use_case/GetNotes.kt
+++ app/src/main/java/com/example/noteapp/feature_note/domain/use_case/GetNotes.kt
@@ -29,9 +29,9 @@ class GetNotes(
                 }
                 is OrderType.Descending ->{
                     when(noteOrder){
-                        is NoteOrder.Title -> notes.sortedBy { it.title.lowercase() }
-                        is NoteOrder.Date -> notes.sortedBy { it.timestamp }
-                        is NoteOrder.Color -> notes.sortedBy { it.color }
+                        is NoteOrder.Title -> notes.sortedByDescending { it.title.lowercase() }
+                        is NoteOrder.Date -> notes.sortedByDescending { it.timestamp }
+                        is NoteOrder.Color -> notes.sortedByDescending { it.color }
                     }
                 }
             }
